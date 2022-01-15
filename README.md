24a330efdc6ae7b3b8fd9301cb2406c0e9afe822
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-01-15 07:57:10 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# QuakeAlert
+
+An android app that will display detailed information on an earthquake of the past and alert the users at the time of an occurring one
---
+++ app/src/main/java/com/def_username/quakealert/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.def_username.quakealert.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.def_username.quakealert.R;
+
+public class MainActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
