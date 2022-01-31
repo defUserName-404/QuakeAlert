cee43cc8232cb8733bbc9c1a90dc59b3031c2ada
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-01-31 12:09:51 +0600

    The app now is navigated through top action bar

--- app/src/main/java/com/def_username/quakealert/view/MainActivity.java
+++ app/src/main/java/com/def_username/quakealert/view/MainActivity.java
@@ -3,6 +3,7 @@ package com.def_username.quakealert.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.def_username.quakealert.R;
 
@@ -12,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setTheme(R.style.Theme_QuakeAlert);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.top_actionbar, menu);
+		return true;
 	}
 }
