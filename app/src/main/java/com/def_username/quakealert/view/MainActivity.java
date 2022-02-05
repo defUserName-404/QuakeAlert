098024fd21f651afc3bd40c10b48fbc41d7420e5
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-02-05 23:16:06 +0600

    Added SearchActivity framework

--- app/src/main/java/com/def_username/quakealert/view/MainActivity.java
+++ app/src/main/java/com/def_username/quakealert/view/MainActivity.java
@@ -2,8 +2,10 @@ package com.def_username.quakealert.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.def_username.quakealert.R;
 
@@ -19,5 +21,10 @@ public class MainActivity extends AppCompatActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.top_actionbar, menu);
 		return true;
+	}
+
+	public void onSearchMenuClicked(MenuItem item) {
+		Intent intent = new Intent(this, SearchActivity.class);
+		startActivity(intent);
 	}
 }
---
+++ app/src/main/java/com/def_username/quakealert/view/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.def_username.quakealert.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.def_username.quakealert.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search);
+	}
+}
